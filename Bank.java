6226459bb2825b28bc8cf025c42a649314806cb9
vaidi24 <miss.vaidi24@gmail.com>
2026-01-12 15:43:08 +0530

    Implement transaction history in Bank class (final)
    Added transaction history functionality to the Bank class, including methods to add and show transactions.

--- Bank.java
+++ Bank.java
@@ -4,13 +4,16 @@ public String name;
 public int age;
 public long mnumber;
 public int balance=0;
+public String[] transaction=new String[100];
+
+public int j=0;
 Customer(String name,int age,long mnumber)
 {
     
     this.name=name;
     this.age=age;
     this.mnumber=mnumber;
-   
+  
 }
 
 }
@@ -41,8 +44,10 @@ public class Bank {
         int accno=scanner.nextInt();
         System.out.println("Enter amount to deposit:");
         int amount=scanner.nextInt();
-        System.out.println("Amount "+amount+" deposited successfully in account number "+accno+" Name "+customer[accno].name);
         customer[accno].balance+=amount;
+        String transaction="Amount "+amount+" deposited successfully in account number "+accno+" Name "+customer[accno].name;
+        System.out.println(transaction);
+        add_transaction(accno, transaction);
         System.out.println("updated balance is "+customer[accno].balance);
 
     }
@@ -52,7 +57,10 @@ public class Bank {
         System.out.println("Enter amount to withdraw:");
         int amount=scanner.nextInt();
         customer[accno].balance-=amount;
-        System.out.println("Amount "+amount+" withdraw successfully from account number "+accno+" Name "+customer[accno].name); 
+        String transaction="Amount "+amount+" withdraw successfully from account number "+accno+" Name "+customer[accno].name;
+        System.out.println(transaction);
+        add_transaction(accno, transaction);
+       // System.out.println("Amount "+amount+" withdraw successfully from account number "+accno+" Name "+customer[accno].name); 
         System.out.println("updated balance is "+customer[accno].balance);
     }
     static void checkbalance(){
@@ -69,10 +77,31 @@ public class Bank {
         int accno2=scanner.nextInt();
         customer[accno1].balance-=amount;
         customer[accno2].balance+=amount;
-        System.out.println("Amount "+amount+" transferred successfully from account number "+accno1+" Name "+customer[accno1].name+"\n to account number "+accno2+" Name "+customer[accno2].name); 
+        String transaction="Amount "+amount+" transferred successfully from account number "+accno1+" Name "+customer[accno1].name+"\n to account number "+accno2+" Name "+customer[accno2].name;
+        System.out.println(transaction); 
+        add_transaction(accno1, transaction);
+        add_transaction(accno2, transaction);
+
         System.out.println("Your updated balance is "+customer[accno1].balance);
     }
+    static void add_transaction(int accno,String transaction)
+    {
+        customer[accno].transaction[customer[accno].j]=transaction;
+        customer[accno].j++;
 
+    }
+    static void show_transaction()
+    {
+        System.out.println("Enter account number:");
+        int accno=scanner.nextInt();
+        int n=0;
+        System.out.println("Transaction history of Account number "+accno);
+        while(n!=customer[accno].j)
+        {
+          System.out.println(customer[accno].transaction[n]);
+          n++;
+        }
+    }
     public static void main(String[] args)
     {   
          
@@ -84,7 +113,7 @@ public class Bank {
         
         do{
 
-          System.out.println("Main menu \n 1. Create Account \n 2. Deposit \n 3. Withdraw \n 4. Check Balance \n 5. Transfer \n 6. Exit \nEnter your choice:-");
+          System.out.println("Main menu \n 1. Create Account \n 2. Deposit \n 3. Withdraw \n 4. Check Balance \n 5. Transfer \n 6. Show transaction history \n 7. Exit \nEnter your choice:-");
           choice=scanner.nextInt();
           if(choice==1)
             {
@@ -106,8 +135,12 @@ public class Bank {
            {
                transfer();
            }
+           else if(choice==6)
+           {
+            show_transaction();
+           }
           
-        }while(choice!=6);
+        }while(choice!=7);
       
         scanner.close();
     }
